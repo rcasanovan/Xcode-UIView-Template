384cb2eefbca6972ff0784e7c36452e513ef2685
Ricardo Casanova <ricardo@pijp.eu>
2019-03-29 19:52:56 +0100

    Add template for UIView

---
+++ UIView.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,57 @@
+//___FILEHEADER___
+
+import UIKit
+
+//__ This class extends UIView. Feel free to modify it if needed
+class ___VARIABLE_moduleName___View: UIView {
+    
+    
+    // MARK: Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupViews()
+    }
+    
+}
+
+// MARK: - Setup views
+extension ___VARIABLE_moduleName___View {
+    
+    /**
+     * Setup views
+     */
+    private func setupViews() {
+        //__ Configure your view here
+        //__ Background color, title, safe area
+        
+        configureSubviews()
+        addSubviews()
+    }
+    
+    /**
+     * Configure subviews
+     */
+    private func configureSubviews() {
+        //__ Configure all the subviews here
+    }
+    
+}
+
+// MARK: - Layout & constraints
+extension ___VARIABLE_moduleName___View {
+    
+    /**
+     * Add subviews
+     */
+    private func addSubviews() {
+        //__ Add all the subviews here
+        
+        //__ Configure the constraints
+    }
+
+}
